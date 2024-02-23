85a39a3f1b7110add8d93542d0165da75d9fd10f
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2024-02-23 21:49:15 +0100

    Otklanjanje bugova

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import $router from '@/router';
 
 let Service = axios.create({
-	baseURL: 'http://localhost:3000',
+	baseURL: 'https://web-aplikacije-projekt-backend.onrender.com',
 	timeout: 1000,
 });
 
