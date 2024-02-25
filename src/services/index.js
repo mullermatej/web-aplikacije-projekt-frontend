49ab776abd575a9f42b8845e28237c848b126e0c
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2024-02-25 13:00:53 +0100

    Backend render url

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import $router from '@/router';
-// baseURL: 'https://web-aplikacije-projekt-backend.onrender.com',
+// baseURL: 'http://localhost:3000/',
 let Service = axios.create({
-	baseURL: 'http://localhost:3000/',
+	baseURL: 'https://web-aplikacije-projekt-backend.onrender.com',
 	timeout: 1000,
 });
 
